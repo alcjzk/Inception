d3f38465d22775d7a1e8c0a3722d549d2eb6918c
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-03-20 06:29:45 +0200

    add password for mysql root user, run healthcheck with unprivileged user

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
 
-ls run
 rm -rf /run/mysqld/
-ls run
 mkdir /run/mysqld/
 chown mysql:mysql /run/mysqld
 
 if [ ! -d "/var/mysql/mysql" ]; then
 
-mariadb-install-db --skip-test-db
-
-fi
-
-if [ ! -d "/var/mysql/${DB_WP_DATABASE}" ]; then
-
 echo "Initializing mariadb.."
+
+mariadb-install-db --skip-test-db
 
 mariadbd --bootstrap << EOF
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
 CREATE DATABASE ${DB_WP_DATABASE};
+CREATE USER 'docker_healthcheck'@'localhost';
 CREATE USER '${DB_WP_USER}'@'%' IDENTIFIED BY '${DB_WP_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_WP_DATABASE}.* TO '${DB_WP_USER}'@'%';
 EOF
--- srcs/requirements/mariadb/tools/install.sh
+++ srcs/requirements/mariadb/tools/install.sh
@@ -3,4 +3,4 @@
 apk update
 apk upgrade
 apk add mysql mysql-client
-
+adduser -s /bin/sh -SDH docker_healthcheck
